0d853c4f2ff93da7d8f46e4dc4215fb314b56f09
QuazarRDesigns <lilymatt2002@gmail.com>
2023-03-26 04:07:31 +1300

    Fix config linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
   env: {
-    es2021: true
+    es2021: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/multi-word-component-names" : "off"
+    "vue/multi-word-component-names" : "off",
   },
   parserOptions: {
     ecmaFeatures: {
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { createVuePlugin as vue } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 const path = require("path");
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-})
-
+  },
+});
