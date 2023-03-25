e83953a50733444f589a54bce339dc3859693c23
QuazarRDesigns <lilymatt2002@gmail.com>
2023-03-26 04:02:51 +1300

    Update ESLint Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,11 @@ module.exports = {
   env: {
     es2021: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/recommended",
-    "@vue/eslint-config-prettier",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/multi-word-component-names" : "off"
   },
   parserOptions: {
     ecmaFeatures: {
