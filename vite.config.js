1da4546427d2da97afae5b6e9ed5f39395d28018
QuazarRDesigns <lilymatt2002@gmail.com>
2024-12-24 01:59:28 +1300

    Convert to ESM

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-const path = require("path");
 export default defineConfig({
   plugins: [vue()],
   resolve: {
